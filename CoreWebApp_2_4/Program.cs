062022966d7d59e2f3ba15468ae6d53f41515a85
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-07-13 17:03:07 +0530

    Test

--- CoreWebApp_2_4/Program.cs
+++ CoreWebApp_2_4/Program.cs
@@ -5,7 +5,6 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 //Add your middlewares
-//app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseEndpoints(endpoints =>
